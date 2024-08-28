7d4388ceb52eda81cfd422222ac2af6c19eff1ee
spageektti <155078792+spageektti@users.noreply.github.com>
2024-08-28 21:21:27 +0200

    add basic /check function

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
 
 app.get('/', (req, res) => {
     res.send('tldr-lint API<br>Copyright (c) 2024 spageektti<br><br>tldr-lint<br>Copyright (c) 2016 Ruben Vereecken<br>Copyright(c) 2016 - present The tldr-pages team and contributors');
+});
+
+app.get('/check/:fileContent', (req, res) => {
+    const fileContent = decodeURIComponent(req.params.fileContent);
+    res.send(fileContent);
 });
